a0d06f1abf1b3089f13eb98bac8dbb2e22e5a678
weijie <weijie.wang@tinno.com>
2017-05-09 17:05:25 +0800

    SimpleConstructor---add Rock2

--- src/com/java/day1/SimpleConstructor.java
+++ src/com/java/day1/SimpleConstructor.java
@@ -5,13 +5,25 @@ package com.java.day1;
  */
 class Rock {
     Rock() {
-        System.out.println("Rock ");
+        System.out.print("Rock ");
+    }
+}
+
+class Rock2 {
+    Rock2(int i) {
+        System.out.print("Rock2 " + i + " ");
     }
 }
 public class SimpleConstructor {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             new Rock();
+        }
+
+        System.out.println("===============");
+
+        for (int i = 0; i < 8; i++) {
+            new Rock2(i);
         }
     }
 }
