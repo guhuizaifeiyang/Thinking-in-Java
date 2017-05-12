8de74464004c496ffd3de64260a7dc6914dd090d
weijie <weijie.wang@tinno.com>
2017-05-12 17:01:49 +0800

    Flower.java

---
+++ src/com/java/chapter5/Flower.java
@@ -0,0 +1,42 @@
+package com.java.chapter5;
+
+/**
+ * Created by weijie on 17-5-12.
+ */
+
+public class Flower {
+    int petalCount = 0;
+    String s = "initial value";
+
+    Flower(int petals) {
+        petalCount = petals;
+        System.out.println("Constructor w/ int arg only, petalCount = " + petalCount);
+    }
+
+    Flower(String ss) {
+        System.out.println("Constructor w/ String arg only, s = " + ss);
+        s = ss;
+    }
+
+    Flower(String s, int petals) {
+        this(petals);
+//        this(s); // Can't call two!
+        this.s = s;
+        System.out.println("String & int args");
+    }
+
+    Flower() {
+        this("hi", 47);
+        System.out.println("default constructor (no args)");
+    }
+
+    void printPetalCount() {
+//        this(11); // Not inside non-constructor
+        System.out.println("petalCount = " + petalCount + " s = " + s);
+    }
+
+    public static void main(String[] args) {
+        Flower x = new Flower();
+        x.printPetalCount();
+    }
+}
