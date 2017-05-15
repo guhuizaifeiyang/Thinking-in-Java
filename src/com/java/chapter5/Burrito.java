ab585053b19647ce52b9a7b5e1975b2d29991f98
weijie <weijie.wang@tinno.com>
2017-05-15 11:43:14 +0800

    Burrito.java

---
+++ src/com/java/chapter5/Burrito.java
@@ -0,0 +1,38 @@
+package com.java.chapter5;
+
+/**
+ * Created by weijie on 17-5-15.
+ */
+public class Burrito {
+    Spiciness degree;
+
+    public Burrito(Spiciness degree) {
+        this.degree = degree;
+    }
+
+    public void describe() {
+        System.out.println("This burrito is ");
+        switch (degree) {
+            case NOT:
+                System.out.println("not spicy at all. ");
+                break;
+            case MILD:
+            case MEDIUM:
+                System.out.println("a little hot");
+                break;
+            case HOT:
+            case FLAMING:
+            default:
+                System.out.println("maybe too hot");
+        }
+    }
+
+    public static void main(String[] args) {
+        Burrito plain = new Burrito(Spiciness.NOT),
+                greenChile = new Burrito(Spiciness.MEDIUM),
+                jalapeno = new Burrito(Spiciness.HOT);
+        plain.describe();
+        greenChile.describe();
+        jalapeno.describe();
+    }
+}
