1d112c30453e77e8a085ad7fac73cb07a5bc0878
weijie <weijie.wang@tinno.com>
2017-05-15 16:43:42 +0800

    Range.java

--- src/com/java/chapter6/PrintTest.java
+++ src/com/java/chapter6/PrintTest.java
@@ -1,15 +1,15 @@
 package com.java.chapter6;
 
-import static net.mindview.util.Print.*;
+import net.mindview.util.Print;
 
 /**
  * Created by weijie on 17-5-15.
  */
 public class PrintTest {
     public static void main(String[] args) {
-        print("Available from now on!");
-        print(100);
-        print(100L);
-        print(3.14159);
+        Print.print("Available from now on!");
+        Print.print(100);
+        Print.print(100L);
+        Print.print(3.14159);
     }
 }
