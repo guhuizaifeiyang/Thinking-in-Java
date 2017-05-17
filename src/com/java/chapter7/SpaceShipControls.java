bcc9f3ce777ff7637d3ed068f014d69e80470eb8
weijie <weijie.wang@tinno.com>
2017-05-17 16:06:29 +0800

    SpaceShipDelegation.java

---
+++ src/com/java/chapter7/SpaceShipControls.java
@@ -0,0 +1,28 @@
+package com.java.chapter7;
+
+/**
+ * Created by weijie on 17-5-17.
+ */
+public class SpaceShipControls {
+    void up(int velocity) {
+    }
+
+    void down(int velocity) {
+    }
+
+    void left(int velocity) {
+    }
+
+    void right(int velocity) {
+    }
+
+    void forward(int velocity) {
+    }
+
+    void back(int velocity) {
+    }
+
+    void turboBoost() {
+
+    }
+}
