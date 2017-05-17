aa5295158f62babfd8f72c624b1c937ed4728e12
weijie <weijie.wang@tinno.com>
2017-05-17 16:27:33 +0800

    Hide.java

---
+++ src/com/java/chapter7/Hide.java
@@ -0,0 +1,35 @@
+package com.java.chapter7;
+
+/**
+ * Created by weijie on 17-5-17.
+ */
+class Homer {
+    char doh(char c) {
+        System.out.println("doh(char)");
+        return 'd';
+    }
+
+    float doh(float f) {
+        System.out.println("doh(float");
+        return 1.0f;
+    }
+}
+
+class Milhouse {
+}
+
+class Bart extends Homer {
+    void doh(Milhouse m) {
+        System.out.println("doh(Milhouse)");
+    }
+}
+
+public class Hide {
+    public static void main(String[] args) {
+        Bart b = new Bart();
+        b.doh(1);
+        b.doh('x');
+        b.doh(1.0f);
+        b.doh(new Milhouse());
+    }
+}
