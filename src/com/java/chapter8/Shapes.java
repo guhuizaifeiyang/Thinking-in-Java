170143bf156f93504f4a988c8270ca6d6d3e4c66
weijie <weijie.wang@tinno.com>
2017-05-31 18:05:53 +0800

    Shapes.java

---
+++ src/com/java/chapter8/Shapes.java
@@ -0,0 +1,80 @@
+package com.java.chapter8;
+
+import java.util.Random;
+
+/**
+ * Created by weijie on 17-5-31.
+ */
+class Shape {
+    public void draw() {
+    }
+
+    public void erase() {
+    }
+}
+
+class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Circle.erase()");
+    }
+}
+
+class Square extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Square.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Square.erase()");
+    }
+}
+
+class Triangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Triangle.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Triangle.erase()");
+    }
+}
+
+class RandomShapeGenerator {
+    private Random rand = new Random(47);
+
+    public Shape next() {
+        switch (rand.nextInt(3)) {
+            default:
+            case 0:
+                return new Circle();
+            case 1:
+                return new Square();
+            case 2:
+                return new Triangle();
+        }
+    }
+}
+
+public class Shapes {
+    private static RandomShapeGenerator gen = new RandomShapeGenerator();
+
+    public static void main(String[] args) {
+        Shape[] s = new Shape[9];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = gen.next();
+        }
+        for (Shape shp : s) {
+            shp.draw();
+        }
+    }
+}
