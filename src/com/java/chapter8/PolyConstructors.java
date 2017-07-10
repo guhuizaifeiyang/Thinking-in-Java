5b03af7273def1d102a46019c5ec706823e3fe2d
weijie <weijie.wang@tinno.com>
2017-07-10 16:39:21 +0800

    PolyConstructors.java

---
+++ src/com/java/chapter8/PolyConstructors.java
@@ -0,0 +1,35 @@
+package com.java.chapter8;
+
+/**
+ * Created by weijie on 17-7-10.
+ */
+class Glyph {
+    void draw() {
+        System.out.println("Glyph.draw()");
+    }
+
+    Glyph() {
+        System.out.println("Glyph() before draw()");
+        draw();
+        System.out.println("Glyph() after draw()");
+    }
+}
+
+class RoundGlyph extends Glyph {
+    private int radius = 1;
+
+    RoundGlyph(int r) {
+        radius = r;
+        System.out.println("RoundGlyph.RoundGlyph(), radius = " + radius);
+    }
+
+    void draw() {
+        System.out.println("RoundGlyph.draw(), radius = " + radius);
+    }
+}
+
+public class PolyConstructors {
+    public static void main(String[] args) {
+        new RoundGlyph(5);
+    }
+}
