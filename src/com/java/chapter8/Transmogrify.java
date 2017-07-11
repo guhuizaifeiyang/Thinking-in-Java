c86dbf91e18b5cb3d1da6a8e2e3e28dd26f29654
weijie <weijie.wang@tinno.com>
2017-07-11 09:57:20 +0800

    Transmogrify.java

---
+++ src/com/java/chapter8/Transmogrify.java
@@ -0,0 +1,42 @@
+package com.java.chapter8;
+
+/**
+ * Created by weijie on 17-7-11.
+ */
+class Actor {
+    public void act() {
+    }
+}
+
+class HappyActor extends Actor {
+    public void act() {
+        System.out.println("HappyActor");
+    }
+}
+
+class SadActor extends Actor {
+    public void act() {
+        System.out.println("SadActor");
+    }
+}
+
+class Stage {
+    private Actor actor = new HappyActor();
+
+    public void change() {
+        actor = new SadActor();
+    }
+
+    public void performPlay() {
+        actor.act();
+    }
+}
+
+public class Transmogrify {
+    public static void main(String[] args) {
+        Stage stage = new Stage();
+        stage.performPlay();
+        stage.change();
+        stage.performPlay();
+    }
+}
