39608a4747660b4e8ca44715593378c5b753990c
weijie <weijie.wang@tinno.com>
2017-07-12 15:21:29 +0800

    TestRandVals.java

---
+++ src/com/java/chapter9/TestRandVals.java
@@ -0,0 +1,11 @@
+package com.java.chapter9;
+
+/**
+ * Created by weijie on 17-7-12.
+ */
+public class TestRandVals {
+    public static void main(String[] args) {
+        System.out.println(RandVals.RANDOM_INT);
+        System.out.println(RandVals.RANDOM_LONG);
+    }
+}
