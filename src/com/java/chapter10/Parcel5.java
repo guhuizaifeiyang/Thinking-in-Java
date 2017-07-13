cd3292b7f1d3d0ca85b90c0098128caac3533cec
weijie <weijie.wang@tinno.com>
2017-07-13 11:27:12 +0800

    Parcel5.java

---
+++ src/com/java/chapter10/Parcel5.java
@@ -0,0 +1,29 @@
+package com.java.chapter10;
+
+/**
+ * Created by weijie on 17-7-13.
+ */
+interface Destination {
+    public String readLabel();
+}
+public class Parcel5 {
+    public Destination destination(String s) {
+        class PDestination implements Destination {
+            private String label;
+
+            private PDestination(String whereTo) {
+                label = whereTo;
+            }
+
+            public String readLabel() {
+                return label;
+            }
+        }
+        return new PDestination(s);
+    }
+
+    public static void main(String[] args) {
+        Parcel5 p = new Parcel5();
+        Destination d = p.destination("Tasmania");
+    }
+}
