9fd2a2e0eb1332e56b548de30a28db4ddb1bdb84
weijie <weijie.wang@tinno.com>
2017-07-14 10:07:45 +0800

    MultiNestingAccess.java

---
+++ src/com/java/chapter10/MultiNestingAccess.java
@@ -0,0 +1,25 @@
+package com.java.chapter10;
+
+/**
+ * Created by weijie on 17-7-14.
+ */
+class MNA {
+    private void f() {}
+    class A {
+        private void g() {}
+        public class B {
+            void h() {
+                g();
+                f();
+            }
+        }
+    }
+}
+public class MultiNestingAccess {
+    public static void main(String[] args) {
+        MNA mna = new MNA();
+        MNA.A mnaa = mna.new A();
+        MNA.A.B mnaab = mnaa.new B();
+        mnaab.h();
+    }
+}
