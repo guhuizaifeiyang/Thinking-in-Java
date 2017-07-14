e1206c8a195f33579fc9bf01bf02dd1180bdf167
weijie <weijie.wang@tinno.com>
2017-07-14 16:46:48 +0800

    PrintingContainers.java

---
+++ src/com/java/chapter11/PrintingContainers.java
@@ -0,0 +1,35 @@
+package com.java.chapter11;
+
+import java.util.*;
+
+/**
+ * Created by weijie on 17-7-14.
+ */
+public class PrintingContainers {
+    static Collection fill(Collection<String> collection) {
+        collection.add("rat");
+        collection.add("cat");
+        collection.add("dog");
+        collection.add("dog");
+        return collection;
+    }
+
+    static Map fill(Map<String, String> map) {
+        map.put("rat", "Fuzzy");
+        map.put("cat", "Rags");
+        map.put("dog", "Bosco");
+        map.put("dog", "Spot");
+        return map;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fill(new ArrayList<String>()));
+        System.out.println(fill(new LinkedList<String>()));
+        System.out.println(fill(new HashSet<String>()));
+        System.out.println(fill(new TreeSet<String>()));
+        System.out.println(fill(new LinkedHashSet<String>()));
+        System.out.println(fill(new HashMap<String, String>()));
+        System.out.println(fill(new TreeMap<String, String>()));
+        System.out.println(fill(new LinkedHashMap<String, String>()));
+    }
+}
