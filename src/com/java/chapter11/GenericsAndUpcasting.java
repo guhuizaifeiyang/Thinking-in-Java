df7c26b334dd6208dba1acfe706e3e35c638e2d2
weijie <weijie.wang@tinno.com>
2017-07-14 15:40:16 +0800

    GenericsAndUpcasting.java

---
+++ src/com/java/chapter11/GenericsAndUpcasting.java
@@ -0,0 +1,24 @@
+package com.java.chapter11;
+
+import java.util.ArrayList;
+
+/**
+ * Created by weijie on 17-7-14.
+ */
+class GrannySmith extends Apple {}
+class Gala extends Apple {}
+class Fuji extends Apple {}
+class Braeburn extends Apple {}
+public class GenericsAndUpcasting {
+    public static void main(String[] args) {
+        ArrayList<Apple> apples = new ArrayList<Apple>();
+        apples.add(new GrannySmith());
+        apples.add(new Gala());
+        apples.add(new Fuji());
+        apples.add(new Braeburn());
+
+        for (Apple c : apples) {
+            System.out.println(c);
+        }
+    }
+}
