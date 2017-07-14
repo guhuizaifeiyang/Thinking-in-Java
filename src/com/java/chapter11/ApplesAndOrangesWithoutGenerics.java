4b2268a7d4f6385524e640b1371676966b79cf36
weijie <weijie.wang@tinno.com>
2017-07-14 15:01:46 +0800

    ApplesAndOrangesWithoutGenerics.java

---
+++ src/com/java/chapter11/ApplesAndOrangesWithoutGenerics.java
@@ -0,0 +1,30 @@
+package com.java.chapter11;
+
+import java.util.ArrayList;
+
+/**
+ * Created by weijie on 17-7-14.
+ */
+class Apple {
+    private static long counter;
+    private final long id = counter++;
+    public long id() {
+        return id;
+    }
+}
+class Orange {}
+public class ApplesAndOrangesWithoutGenerics {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        ArrayList apples = new ArrayList();
+        for (int i = 0; i < 3; i++) {
+            apples.add(new Apple());
+        }
+        // Not prevented from adding an Orange to apples:
+        apples.add(new Orange());
+        for (int i = 0; i < apples.size(); i++) {
+            ((Apple)apples.get(i)).id();
+            // Orange is detected only at run time
+        }
+    }
+}
