e52f56b5b0ae1f090dc4e43759b17a73915c7b86
weijie <weijie.wang@tinno.com>
2017-07-17 14:10:34 +0800

    FullConstructors.java

---
+++ src/com/java/chapter12/FullConstructors.java
@@ -0,0 +1,34 @@
+package com.java.chapter12;
+
+/**
+ * Created by weijie on 17-7-17.
+ */
+class MyException extends Exception {
+    public MyException() {}
+    public MyException(String msg) { super(msg);}
+}
+
+public class FullConstructors {
+    public static void f() throws MyException {
+        System.out.println("Throwing MyException from f()");
+        throw new MyException();
+    }
+    public static void g() throws MyException {
+        System.out.println("Throwing MyException from g()");
+        throw new MyException();
+    }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            g();
+        } catch (MyException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
