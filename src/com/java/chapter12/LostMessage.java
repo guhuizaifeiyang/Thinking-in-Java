4e7b1a60bc8049f7a1a8307614586ccc0f91f728
weijie <weijie.wang@tinno.com>
2017-07-20 16:33:35 +0800

    LostMessage.java

---
+++ src/com/java/chapter12/LostMessage.java
@@ -0,0 +1,38 @@
+package com.java.chapter12;
+
+/**
+ * Created by weijie on 17-7-20.
+ */
+class VeryImportantException extends Exception {
+    @Override
+    public String toString() {
+        return "A very import exception";
+    }
+}
+class HoHumException extends Exception {
+    @Override
+    public String toString() {
+        return "A trivial exception";
+    }
+}
+public class LostMessage {
+    void f() throws VeryImportantException {
+        throw new VeryImportantException();
+    }
+    void dispose() throws HoHumException {
+        throw new HoHumException();
+    }
+
+    public static void main(String[] args) {
+        try {
+            LostMessage lm = new LostMessage();
+            try {
+                lm.f();
+            } finally {
+                lm.dispose();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
